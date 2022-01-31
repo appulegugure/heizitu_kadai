d209693edce597874a5d5a49167094b8cb86481a
TA <appulegugure@gmail.com>
2022-02-01 08:54:51 +0900

    2/1:09:00修正

--- kadai_6_1.py
+++ kadai_6_1.py
@@ -20,15 +20,15 @@ def init_db():
     # 実行状態を保存
     conn.commit()
     # コネクションを閉じる。
-    conn.close()
+    # conn.close()
 
 
-def all_userinfo():
+def all_userinfo(conn, cur):
     # DBの情報を取得
-    dsn = os.environ.get('DATABASE_URL')
+    # dsn = os.environ.get('DATABASE_URL')
     # DBに接続(コネクションを貼る)
-    conn = psycopg2.connect(dsn)
-    cur = conn.cursor()
+    # conn = psycopg2.connect(dsn)
+    # cur = conn.cursor()
     # SQLを用意
     with open('sql_dir/all_userinfo.sql', encoding="utf-8") as f:
         sql = f.read()
@@ -39,16 +39,16 @@ def all_userinfo():
     # 実行状態を保存
     conn.commit()
     # コネクションを閉じる。
-    conn.close()
+    # conn.close()
     print('\n'.join([f"Name: {i[0]} Age: {i[1]}" for i in fetch_you]))
 
 
-def add_user(name, age):
+def add_user(name, age, conn, cur):
     # DBの情報を取得
-    dsn = os.environ.get('DATABASE_URL')
+    # dsn = os.environ.get('DATABASE_URL')
     # DBに接続(コネクションを貼る)
-    conn = psycopg2.connect(dsn)
-    cur = conn.cursor()
+    # conn = psycopg2.connect(dsn)
+    # cur = conn.cursor()
     # SQLを用意
     with open('sql_dir/insert.sql', encoding="utf-8") as f:
         sql = f.read()
@@ -57,17 +57,17 @@ def add_user(name, age):
     # 実行状態を保存
     conn.commit()
     # コネクションを閉じる。
-    conn.close()
+    # conn.close()
 
     print(f"add new user {name}")
 
 
-def users_find(name):
+def users_find(name, conn, cur):
     # DBの情報を取得
-    dsn = os.environ.get('DATABASE_URL')
+    # dsn = os.environ.get('DATABASE_URL')
     # DBに接続(コネクションを貼る)
-    conn = psycopg2.connect(dsn)
-    cur = conn.cursor()
+    # conn = psycopg2.connect(dsn)
+    # cur = conn.cursor()
     # SQLを用意
 
     with open('sql_dir/user_find.sql', encoding="utf-8") as f:
@@ -86,16 +86,16 @@ def users_find(name):
     # 実行状態を保存
     conn.commit()
     # コネクションを閉じる。
-    conn.close()
+    # conn.close()
     print('\n'.join([f"Name: {i[0]} Age: {i[1]}" for i in fetch_you]))
 
 
-def delete_user(name):
+def delete_user(name, conn, cur):
     # DBの情報を取得
-    dsn = os.environ.get('DATABASE_URL')
+    # dsn = os.environ.get('DATABASE_URL')
     # DBに接続(コネクションを貼る)
-    conn = psycopg2.connect(dsn)
-    cur = conn.cursor()
+    # conn = psycopg2.connect(dsn)
+    # cur = conn.cursor()
     # SQLを用意
     with open('sql_dir/user_delete.sql', encoding="utf-8") as f:
         sql = f.read()
@@ -104,16 +104,16 @@ def delete_user(name):
     # 実行状態を保存
     conn.commit()
     # コネクションを閉じる。
-    conn.close()
+    # conn.close()
 
     print(f"delete  user {name}")
 
 
-def edit_user(name, age):
-    dsn = os.environ.get('DATABASE_URL')
+def edit_user(name, age, conn, cur):
+    # dsn = os.environ.get('DATABASE_URL')
     # DBに接続(コネクションを貼る)
-    conn = psycopg2.connect(dsn)
-    cur = conn.cursor()
+    # conn = psycopg2.connect(dsn)
+    # cur = conn.cursor()
     # SQLを用意
     with open('sql_dir/edit_user.sql', encoding="utf-8") as f:
         sql = f.read()
@@ -122,13 +122,18 @@ def edit_user(name, age):
     # 実行状態を保存
     conn.commit()
     # コネクションを閉じる。
-    conn.close()
+    # conn.close()
 
     print(f"edit user finish")
 
 
 def main():
     init_db()
+    # databaseのURLを取得
+    dsn = os.environ.get('DATABASE_URL')
+    # DBに接続(コネクションを貼る)
+    conn = psycopg2.connect(dsn)
+    # cur = conn.cursor()
 
     with open('print_introduction.txt', encoding="utf-8") as introduction:
 
@@ -137,33 +142,42 @@ def main():
     while True:
         command = str(input('どんな操作')).upper()
         if command == 'A':
+
             print('A')
             Name = str(input("名前を入力してください。> "))
             Age = int(input("年齢を入力して下さい > "))
-            add_user(Name, Age)
+            cur = conn.cursor()
+            add_user(Name, Age, conn, cur)
 
         elif command == 'S':
+
             print('S')
-            all_userinfo()
+            cur = conn.cursor()
+            all_userinfo(conn, cur)
 
         elif command == 'F':
+
             Name = str(input("検索する名前を入力してください。> "))
             print('F')
-            users_find(Name)
+            cur = conn.cursor()
+            users_find(Name, conn, cur)
 
         elif command == 'Q':
             print('Bye!')
+            conn.close()
             break
 
         elif command == 'D':
             Name = str(input("削除する名前を入力してください。> "))
             print('D')
-            delete_user(Name)
+            cur = conn.cursor()
+            delete_user(Name, conn, cur)
 
         elif command == 'E':
             Name = str(input("編集する人を入力してください。 > "))
             Age = input("age? > ")
-            edit_user(Name, Age)
+            cur = conn.cursor()
+            edit_user(Name, Age, conn, cur)
 
         else:
             print(f"{command}: command not found")
