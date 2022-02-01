0f2a98b197f7bacd72adb5d3a6a8a89a0ec91c02
TA <appulegugure@gmail.com>
2022-02-01 23:05:46 +0900

    Find機能修正

--- kadai_6_1.py
+++ kadai_6_1.py
@@ -14,7 +14,7 @@ def init_db(conn, cur):
     # 実行状態を保存
     conn.commit()
     
-    return print("CREATE TABLE")
+    return 0
 
 
 def all_userinfo(conn, cur):
@@ -27,7 +27,7 @@ def all_userinfo(conn, cur):
     # 実行状態を保存
     conn.commit()
 
-    return print('\n'.join([f"Name: {i[0]} Age: {i[1]}" for i in fetch_user]))
+    return print('\n'.join([f"Name: {i[0]} Age: {i[1]}" for i in fetch_user]) + '\n')
 
 
 def add_user(name, age, conn, cur):
@@ -39,7 +39,7 @@ def add_user(name, age, conn, cur):
     # 実行状態を保存
     conn.commit()
 
-    return print(f"add new user {name}")
+    return print(f"add new user {name} \n")
 
 
 def users_find(name, conn, cur):
@@ -56,7 +56,7 @@ def users_find(name, conn, cur):
     if len(fetch_user) == 0:
         return print(f"Sorry, {name} is not found")
     else:
-        return print('\n'.join([f"Name: {i[0]} Age: {i[1]}" for i in fetch_user]))
+        return print(['\n'.join([f"Name: {i[0]} Age: {i[1]}" for i in fetch_user])] + '\n')
 
 
 def delete_user(name, conn, cur):
@@ -68,7 +68,7 @@ def delete_user(name, conn, cur):
     # 実行状態を保存
     conn.commit()
 
-    return print(f"delete  user {name}")
+    return print(f"delete  user {name} \n")
 
 
 def edit_user(name, edit_name, edit_age, conn, cur):
@@ -80,7 +80,7 @@ def edit_user(name, edit_name, edit_age, conn, cur):
     # 実行状態を保存
     conn.commit()
 
-    return print(f"update user {edit_name}")
+    return print(f"update user {edit_name} \n")
 
 
 def main():
@@ -101,27 +101,42 @@ def main():
 
     # DB OPERATION START POINT
     while True:
-        command = str(input('どんな操作')).upper()
+        command = str(input('\nChoose a operation? > ')).upper()
         if command == 'A':
 
-            print('A')
-            Name = str(input("名前を入力してください。> "))
-            Age = int(input("年齢を入力して下さい > "))
+            print('=== Add new user ===')
+            name = str(input("Please enter new user > "))
+            if name == '!q': continue
+
+            age = int(input("Please enter user age > "))
+            if age == '!q': continue
+
             # cur = conn.cursor()
-            add_user(Name, Age, conn, cur)
+            try:
+                add_user(name, age, conn, cur)
+            except psycopg2.errors.UniqueViolation:
+                print(f"Duplicated user name: {name}")
+            else:
+                print(f"Add new user: {name}")
+            finally:
+                continue
 
         elif command == 'S':
 
-            print('S')
+            print('=== Show all users inf ===')
             # cur = conn.cursor()
+
             all_userinfo(conn, cur)
+
 
         elif command == 'F':
 
-            Name = str(input("検索する名前を入力してください。> "))
-            print('F')
+            print('=== Find user information ===')
+            name = str(input("Please enter find user > "))
+            if name == '!q': continue
+
             # cur = conn.cursor()
-            users_find(Name, conn, cur)
+            users_find(name, conn, cur)
 
         elif command == 'Q':
             print('Bye!')
@@ -129,17 +144,27 @@ def main():
             break
 
         elif command == 'D':
-            Name = str(input("削除する名前を入力してください。> "))
-            print('D')
+
+            print('=== delete user in users table ===')
+            name = str(input("Please enter delete user > "))
+            if name == '!q': continue
+
             # cur = conn.cursor()
-            delete_user(Name, conn, cur)
+            delete_user(name, conn, cur)
 
         elif command == 'E':
-            Name = str(input("編集する人を入力してください。 > "))
+            print("=== Edit user ===")
+            name = str(input("Please enter edit user > "))
+            if name == '!q': continue
+
             edit_name = str(input("name? > "))
+            if edit_name == '!q': continue
+
             edit_age = input("age? > ")
+            if edit_age == '!q': continue
+
             # cur = conn.cursor()
-            edit_user(Name, edit_name, edit_age, conn, cur)
+            edit_user(name, edit_name, edit_age, conn, cur)
 
         else:
             print(f"{command}: command not found")
